8508dd23fbc1acbdbdebd507139390a295e86606
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-03-13 11:21:44 -0300

    Commit del día 13-03-2023. El error para conectar en la base de datos persiste

--- src/main/java/com/example/CalculadorIVA/repositorios/DeduccionRepositorio.java
+++ src/main/java/com/example/CalculadorIVA/repositorios/DeduccionRepositorio.java
@@ -2,7 +2,9 @@ package com.example.CalculadorIVA.repositorios;
 
 import com.example.CalculadorIVA.entidades.Deduccion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeduccionRepositorio extends JpaRepository<Deduccion, String> {
 
 }
