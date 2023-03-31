ccf89f832365d2af57ce59672d4eac9809c2b4f2
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-03-31 15:42:00 -0300

    Reinicio del proyecto

---
+++ src/test/java/com/example/calculadoraiva/CalculadoraIvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculadoraiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraIvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
