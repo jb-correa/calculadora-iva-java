2c65f0b31ad82ab9e0f1fc30cf3e2e632f800150
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-10-31 11:01:04 -0300

    Reinicio del proyecto con Spring Initializr

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.calculadoraspring {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.calculadoraspring to javafx.fxml;
-    exports com.example.calculadoraspring;
-}
