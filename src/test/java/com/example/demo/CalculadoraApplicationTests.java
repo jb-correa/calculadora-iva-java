b0fec71cfbb52bd57daee2c378d881a7049b026a
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-11-09 10:51:07 -0300

    Trabajo en JUnit testing

--- src/test/java/com/example/demo/CalculadoraApplicationTests.java
+++ src/test/java/com/example/demo/CalculadoraApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class CalculadoraApplicationTests {
 
+
 	@Test
 	void contextLoads() {
+
+
+
 	}
 
 }
