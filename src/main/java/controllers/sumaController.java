629a86cc5ddcc57bb8c6c1362c88d8d90fc64fec
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-11-09 11:21:57 -0300

    Trabajo en htmls

---
+++ src/main/java/controllers/sumaController.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import services.SumaService;
+
+@Controller
+@RequestMapping("/suma")
+public class sumaController {
+
+    @Autowired
+    public SumaService sumaService;
+
+    @GetMapping("/")
+    public String suma (){
+
+        return "templates/suma.html";
+    }
+
+    @PostMapping("/")
+    public String sumar(@RequestParam double precio, @RequestParam double porcentaje){
+
+        sumaService.crearSuma(precio, porcentaje);
+
+        return "templates/suma.html";
+    }
+
+}
