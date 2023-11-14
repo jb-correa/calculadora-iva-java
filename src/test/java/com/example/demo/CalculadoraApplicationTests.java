c2dc28b7285c84386d5f655585d89032ebf9166a
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-11-14 10:48:41 -0300

    Trabajo en JUnit Tests

--- src/test/java/com/example/demo/CalculadoraApplicationTests.java
+++ src/test/java/com/example/demo/CalculadoraApplicationTests.java
@@ -1,17 +1,28 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import services.SumaService;
 
 
 @SpringBootTest
 class CalculadoraApplicationTests {
 
+	@Bean
+	
+
 
 	@Test
 	void contextLoads() {
 
+	}
 
+	@Test
+	void creacionSuma(){
+
+		sumaService.crearSuma(100, 21);
 
 	}
 
